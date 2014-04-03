e1d03124374b94419cd56e16b9d4d59325f2293c
Igor A. Perminov <igor.a.perminov@gmail.com>
2014-04-04 01:24:56 +0400

    ARM: GPIO routines for PXA architecture are re-implemented using readl/writel
    Signed-off-by: Igor A. Perminov <igor.a.perminov@gmail.com>

--- arch/arm/include/asm/arch-pxa/gpio.h
+++ arch/arm/include/asm/arch-pxa/gpio.h
@@ -9,6 +9,7 @@
 #ifndef	__ASM_PXA_GPIO_H__
 #define	__ASM_PXA_GPIO_H__
 
+#include <asm/io.h>
 #include <asm/types.h>
 #include "pxa-regs.h"
 
@@ -21,27 +22,25 @@ static inline int gpio_free(unsigned gpio)
 { return 0; }
 
 static inline int gpio_direction_input(unsigned gpio) {
-	*((volatile u32 *) GPDR(gpio)) &= ~GPIO_bit(gpio);
+	writel(readl(GPDR(gpio)) & ~GPIO_bit(gpio), GPDR(gpio));
 	return 0;
 }
 
 static inline int gpio_direction_output(unsigned gpio, int value) {
 	gpio_set_value(gpio, value);
-	*((volatile u32 *) GPDR(gpio)) |= GPIO_bit(gpio);
+	writel(readl(GPDR(gpio)) | GPIO_bit(gpio), GPDR(gpio));
 	return 0;
 }
 
 static inline int gpio_get_value(unsigned gpio) {
-	return !!(*((volatile u32 *) GPLR(gpio)) & GPIO_bit(gpio));
+	return !!(readl(GPLR(gpio)) & GPIO_bit(gpio));
 }
 
 static inline int gpio_set_value(unsigned gpio, int value) {
-	if (value) {
-		*((volatile u32 *) GPSR(gpio)) |= GPIO_bit(gpio);
-	}
-	else {
-		*((volatile u32 *) GPCR(gpio)) |= GPIO_bit(gpio);
-	}
+	if (value)
+		writel(GPIO_bit(gpio), GPSR(gpio));
+	else
+		writel(GPIO_bit(gpio), GPCR(gpio));
 	return 0;
 }
 
